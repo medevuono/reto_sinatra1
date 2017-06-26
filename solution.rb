4b97f211ca1e304574121cb704233460b70d91c9
Maria De Vuono <medevuono@gmail.com>
2017-06-26 16:02:04 -0500

    Solucion de Saludame 1 de Make it real

--- solution.rb
+++ solution.rb
@@ -2,12 +2,18 @@ require "sinatra.rb"
 
 get "/" do
   unless params[:name]
-    "Hola desconocido!"
+    <<-HTML
+      <h1>Hola desconocido!</h1>
+    HTML
   else
     if params[:name].empty?
-      "Hola desconocido!"
+      <<-HTML
+        <h1>Hola desconocido!</h1>
+      HTML
     else
-      "Hola #{params[:name]}!"
+      <<-HTML 
+        <h1>"Hola #{params[:name]}!"</h1>
+      HTML
     end    
   end
 end
