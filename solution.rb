85e449e359635fdb717498a95c56b1555d168427
Maria De Vuono <medevuono@gmail.com>
2017-06-27 10:14:42 -0500

    Solucion de Saludame 2 de Make it real

--- solution.rb
+++ solution.rb
@@ -1,22 +1,22 @@
 require "sinatra.rb"
 
-# get "/" do
-#   unless params[:nombre]
-#     <<-HTML
-#       <h1>Hola desconocido!</h1>
-#     HTML
-#   else
-#     if params[:name].empty?
-#       <<-HTML
-#         <h1>Hola desconocido!</h1>
-#       HTML
-#     else
-#       <<-HTML 
-#         <h1>Hola #{params[:nombre]}!</h1>
-#       HTML
-#     end    
-#   end
-# end
+get "/" do
+  unless params[:nombre]
+    <<-HTML
+      <h1>Hola desconocido!</h1>
+    HTML
+  else
+    if params[:name].empty?
+      <<-HTML
+        <h1>Hola desconocido!</h1>
+      HTML
+    else
+      <<-HTML 
+        <h1>Hola #{params[:nombre]}!</h1>
+      HTML
+    end    
+  end
+end
 
 get "/makers/juan" do 
   <<-HTML
